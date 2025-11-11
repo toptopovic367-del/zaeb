c83396338584b30e1c0c00b9e699b0f5828598b0
toptopovic367-del <toptopovic367@gmail.com>
2025-11-12 00:04:17 +0300

    feat: added new command

--- main.py
+++ main.py
@@ -370,4 +370,4 @@ def cancel_delete(message):
 # Запуск бота
 if __name__ == '__main__':
     print("Бот запущен!")
-    bot.polling(none_stop=True)
+    bot.polling(none_stop=True, skip_pending=True)
